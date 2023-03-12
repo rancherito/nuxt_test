8fb8b2bfd79a889153ccd792f355a29962bc23d4
CoffeeDuck <davidlive0159@gmail.com>
2023-03-12 05:40:08 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,9 @@
+<script lang="ts">
+import { Testeo } from "./.nuxt/components";
+</script>
+<template>
+	<div>
+		<Testeo></Testeo>
+		<Testeo></Testeo>
+	</div>
+</template>
---
+++ components/testeo.vue
@@ -0,0 +1,29 @@
+<script lang="ts">
+import { ref } from 'vue';
+</script>
+<template>
+	<div>
+		<h1>Welcome to the homepage</h1>
+		<p>{{ data.name }}</p>
+		{{ message }}
+		<br>
+		cangrejo
+	</div>
+</template>
+<script setup lang="ts">
+//add data
+const data = reactive({
+	name: 'John Doe',
+	message: 'Hello World'
+})
+const message = ref('CAFE CON PATO')
+//mount data
+onMounted(() => {
+	console.log(data.name)
+	data.name = 'Jane Doe'
+	//for 10 numbers
+	for (let i = 0; i < 10; i++) {
+		console.log(i)
+	}
+})
+</script>
