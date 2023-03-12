aa6b0b4cad937634cdf45b8beaa983edbe0bffa8
CoffeeDuck <davidlive0159@gmail.com>
2023-03-12 06:54:42 -0500

    up

--- app.vue
+++ app.vue
@@ -1,9 +1,21 @@
 <script lang="ts">
 import { Testeo } from "./.nuxt/components";
+import Button from "primevue/button"
 </script>
 <template>
 	<div>
 		<Testeo></Testeo>
 		<Testeo></Testeo>
+		<Button label="jamon" size="small"></Button>
+
+		<Card>
+			<template #title> Simple Card </template>
+			<template #content>
+				Lorem ipsum dolor sit amet, consectetur adipisicing elit. Inventore sed consequuntur error repudiandae
+				numquam deserunt quisquam repellat libero asperiores earum nam nobis, culpa ratione quam perferendis
+				esse, cupiditate neque
+				quas!
+			</template>
+		</Card>
 	</div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,26 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const baseUrl = '/';
 export default defineNuxtConfig({
+	css: [
+		//core
+		"primevue/resources/primevue.min.css",
+		//icons
+		"primeicons/primeicons.css"
+	],
+	build: {
+		transpile: ["primevue"]
+	},
+	app: {
+		baseURL: baseUrl,
+		head: {
+			link: [
+				{
+					id: 'theme-link',
+					rel: 'stylesheet',
+					href: baseUrl + 'theme.css'
+				}
+			]
+		}
+	}
 
 })
